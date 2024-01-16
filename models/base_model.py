84c3c03f577f626d502014178f71b7f39b095999
AhmedHosny1998 <hosny4620Qgmail.com>
2024-01-16 14:51:49 -0800

    clone

--- models/base_model.py
+++ models/base_model.py
@@ -2,18 +2,18 @@
 """
 Module for the BaseModel class.
 """
-from datetime import datetime
 import uuid
-from models import storage
+from datetime import datetime
+import models
+
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
@@ -21,34 +21,34 @@ class BaseModel:
                 elif key == "created_at" or key == "updated_at":
                     setattr(self, key, datetime.strptime(value, time_format))
                 else:
-                    setattr(self,key, value)
-    models.storage.new(self)
+                    setattr(self, key, value)
+
+        models.storage.new(self)
 
     def save(self):
-        """Updates the updated_at attribute
-        with the current datetime."""
+        """
 
-        self.updated_at = datetime.now()
+        """
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
-         """Returns a dictionary representation of an instance."""
+        """
 
-         my_dict = self.__dict__.copy()
-         my_dict["__class__"] = self.__class__.__name__
-         my_dict["created_at"] = self.created_at.isoformat()
-         my_dict["updated_at"] = self.updated_at.isoformat()
+        """
+        inst_dict = self.__dict__.copy()
+        inst_dict["__class__"] = self.__class__.__name__
+        inst_dict["created_at"] = self.created_at.isoformat()
+        inst_dict["updated_at"] = self.updated_at.isoformat()
 
-         return my_dict
-         
-        
+        return inst_dict
+
     def __str__(self):
-        """Returns a human-readable string representation
-        of an instance."""
+        """
 
-        my_class = self.__class__.__name__
-        return "[{}] ({}) {}".format(my_class, self.id, self.__dict__)
-
+        """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
 
 if __name__ == "__main__":
@@ -65,4 +65,11 @@ if __name__ == "__main__":
     for key in my_model_json.keys():
         print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
 
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
 
+    print("--")
+    print(my_model is my_new_model)
